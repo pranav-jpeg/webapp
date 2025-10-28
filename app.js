a9a8acc2127e839c848bbcc3f189132d3043a164
Pranav <pranavbhagwat17@gmail.com>
2025-10-29 01:00:23 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const path = require('path');
+const bodyParser = require('body-parser');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,12 +14,20 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 const userSchema = new mongoose.Schema({ username: String, email: String });
 const User = mongoose.model('User', userSchema);
 
-app.post('/submit', (req, res) => {
-  const newUser = new User({ username: req.body.username, email: req.body.email });
-  newUser.save().then(() => res.send('Saved to database')).catch(() => res.send('Error'));
-});
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('Server started'));
+app.post('/submit', (req, res) => {
+  const newUser = new User({ username: req.body.username, email: req.body.email });
+  newUser.save()
+    .then(() => res.send('Saved to database'))
+    .catch(() => res.send('Error'));
+});
+
+const port = process.env.PORT || 3000;
+
+console.log('Static folder:', path.join(__dirname, 'public'));
+console.log('Starting server on port:', port);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
