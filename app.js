25cf40544717cc1cc55b90abbc490a2b0433d3b1
Pranav <pranavbhagwat17@gmail.com>
2025-10-28 23:32:13 +0530

    Prepare for deployment

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema({ username: String, email: String });
+const User = mongoose.model('User', userSchema);
+
+app.post('/submit', (req, res) => {
+  const newUser = new User({ username: req.body.username, email: req.body.email });
+  newUser.save().then(() => res.send('Saved to database')).catch(() => res.send('Error'));
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('Server started'));
